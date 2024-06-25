3b33e5d135477acba6ec7315debad4da152b9905
JunDemi <jungwook3176@gmail.com>
2024-06-25 15:51:15 +0900

    프로젝트 생성

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function App() {
+function Game() {
   return (
    <div>
     Hello
@@ -8,4 +8,4 @@ function App() {
   );
 }
 
-export default App;
+export default Game;

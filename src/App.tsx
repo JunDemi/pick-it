4d3351b1d0ad10cd29ecd1b384b5f6607c0f1dbc
JunDemi <jungwook3176@gmail.com>
2024-06-26 15:31:43 +0900

    fix: Readme 변경 및 프로젝트 폴더 구조 빌드

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './pages/Home';
 
 function App() {
   return (
-   <div>
-    Hello
-   </div>
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 

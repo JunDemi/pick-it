45a5e96dca06bf0b57a7a35fc379315d17ee628a
JunDemi <jungwook3176@gmail.com>
2024-06-27 18:00:29 +0900

    Feat: 다중이미지 인풋 구현, 각 파일 용량에 대한 유효성 검사

--- src/pages/CreateGame.tsx
+++ src/pages/CreateGame.tsx
@@ -1,20 +1,35 @@
+import { useState } from "react";
 import "../assets/CreateGame/style.scss";
 
 function CreateGame() {
+
+  const [imageList, setImageList] = useState<File[]>([]);
+
+  //드래그 오버
   const handleDragOver = (event: any) => {
-    event.preventDefault();
+    event.preventDefault(); //브라우저에 이미지 새 창이 뜨는 동작 제한
     console.log("Drag Over");
   };
-
+  //드롭
   const handleDrop = (event: any) => {
-    event.preventDefault();
-    const file = event.dataTransfer.files[0];
-
-    console.log(file);
-
-    // 드롭된 파일 핸들링
-    // ...
+    const middleList = [...imageList]; //함수 안에 임시적으로 저장되는 파일 리스트
+    event.preventDefault(); //브라우저에 이미지 새 창이 뜨는 동작 제한
+    const fileList = event.dataTransfer.files;
+    for(let i = 0; i < fileList.length; i++){ 
+      if(fileList[i].size >= 3145728){ //파일 크기가 3MB 이상일 때 동작 제한
+        continue; //다음 루프로 스킵
+      }else{
+        middleList.push(fileList[i]);
+      }
+    }
+    setImageList(middleList);
   };
+
+  console.log(imageList);
+  const handleFile = (event: any) => {
+    const file = event.target.files[0];
+    console.log(file);
+  }
   return (
     <>
       <label
@@ -22,8 +37,9 @@ function CreateGame() {
         onDragOver={handleDragOver}
         onDragLeave={() => console.log("Drag Leave")}
         onDrop={handleDrop}
+        onChange={handleFile}
       >
-        <input type="file" className="myfile" accept="image/*" />
+        <input type="file" className="myfile" accept="image/*" multiple/>
         <h3>드래그</h3>
       </label>
     </>

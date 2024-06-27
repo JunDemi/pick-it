2d59bbf2468d3788aab1463e231de2749e5f273d
JunDemi <jungwook3176@gmail.com>
2024-06-27 17:03:34 +0900

    Fix: react hook form 라이브러리 설치, Feat: 파일 드래그 앤 드롭 프로토타입 구현

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
 import '../src/assets/global.scss';
+import CreateGame from './pages/CreateGame';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header/>
       <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='/addgame' element={<CreateGame/>}/>
       </Routes>
     </BrowserRouter>
   );

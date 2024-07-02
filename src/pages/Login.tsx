c4b0b056b41fa3160de345da1026405049afe12a
JunDemi <jungwook3176@gmail.com>
2024-07-02 17:59:30 +0900

    Feat: 로그인 페이지 1차. InputField 컴포넌트 아토믹 디자인 구성

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React from "react";
+import "../assets/Sign/sign.scss";
+import { useForm } from "react-hook-form";
+import InputField from "../components/Sign/InputField";
 
 function Login() {
+    const {handleSubmit, register, formState:{errors}, reset} = useForm({ mode: "all" });
   return (
-   <div>
-    Hello
-   </div>
+    <section className="sign-container">
+      <img
+        src="/images/introduce.png"
+        alt="소개이미지"
+        className="sign-introduce"
+      />
+      <div className="login-container">
+        <div className="login-form-top">
+          <h1>로그인</h1>
+          <div>
+            <span>아이디 찾기</span>
+            <span>비밀번호 찾기</span>
+          </div>
+        </div>
+        <form className="login-form-section">
+            <InputField name="loginId" placeholder="아이디" register={register} error={errors.loginId}/>
+        </form>
+      </div>
+    </section>
   );
 }
 

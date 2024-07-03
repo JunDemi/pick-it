089cbd856251452cd431373c97a6ab08be9c6d7e
JunDemi <jungwook3176@gmail.com>
2024-07-03 18:43:41 +0900

    Feat: 로그인, 회원가입 UI 구현

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,9 +2,20 @@ import React from "react";
 import "../assets/Sign/sign.scss";
 import { useForm } from "react-hook-form";
 import InputField from "../components/Sign/InputField";
+import { Link } from "react-router-dom";
+import { LoginType } from "../types/Sign";
 
 function Login() {
-    const {handleSubmit, register, formState:{errors}, reset} = useForm({ mode: "all" });
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+    reset,
+  } = useForm<LoginType>({ mode: "onSubmit" });
+
+  const onLoginValid = (data: LoginType) => {
+    console.log(data);
+  };
   return (
     <section className="sign-container">
       <img
@@ -20,9 +31,28 @@ function Login() {
             <span>비밀번호 찾기</span>
           </div>
         </div>
-        <form className="login-form-section">
-            <InputField name="loginId" placeholder="아이디" register={register} error={errors.loginId}/>
+        <form
+          className="login-form-section"
+          onSubmit={handleSubmit(onLoginValid)}
+        >
+          <InputField
+            name="loginId"
+            placeholder="아이디"
+            register={register}
+            error={errors.loginId}
+          />
+          <InputField
+            name="loginPw"
+            placeholder="비밀번호"
+            register={register}
+            error={errors.loginPw}
+          />
+          <button type="submit">로그인</button>
         </form>
+        <div className="login-form-bottom">
+          <p>계정이 없으신가요? PICKIT 커뮤니티에 참여해주세요!</p>
+          <Link to="/register">계정 생성</Link>
+        </div>
       </div>
     </section>
   );
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,10 +1,77 @@
-import React from 'react';
+import React from "react";
+import { useForm } from "react-hook-form";
+import "../assets/Sign/sign.scss";
+import InputField from "../components/Sign/InputField";
+import { Link } from "react-router-dom";
+import { RegisterType } from "../types/Sign";
 
 function Register() {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+    reset,
+  } = useForm<RegisterType>({ mode: "onSubmit" });
+
+  const onRegisterValid = (data: RegisterType) => {
+    console.log(data);
+  };
   return (
-   <div>
-    Hello
-   </div>
+    <section className="sign-container">
+      <img
+        src="/images/introduce.png"
+        alt="소개이미지"
+        className="sign-introduce"
+      />
+      <div className="register-container">
+        <h1 className="register-form-top">계정 생성</h1>
+        <form
+          className="register-form-section"
+          onSubmit={handleSubmit(onRegisterValid)}
+        >
+          <table>
+            <tbody>
+            <tr>
+              <td>
+                <h1>닉네임</h1>
+              </td>
+              <td>
+                <InputField name="registerNickName" register={register} error={errors.registerNickName}/>
+              </td>
+              <td>
+                <button className="nickname-check">중복확인</button>
+              </td>
+              <td><p>*(영문소문자/숫자, 4~16자)</p></td>
+            </tr>
+
+            <tr>
+              <td>
+                <h1>아이디</h1>
+              </td>
+              <td colSpan={3}>
+              <InputField name="registerId" register={register} error={errors.registerId}/>
+              </td>
+            </tr>
+
+            <tr>
+              <td>
+                <h1>비밀번호</h1>
+              </td>
+              <td colSpan={2}>
+              <InputField name="registerPw" register={register} error={errors.registerPw}/>
+              </td>
+              <td><p>*(영문대소문자/숫자, 10~16자)</p></td>
+            </tr>
+            </tbody>
+          </table>
+          <button className="register-submit" type="submit">계정 생성</button>
+        </form>
+        <div className="register-form-bottom">
+          <p>계정이 없으신가요? PICKIT 커뮤니티에 참여해주세요!</p>
+          <Link to="/login">로그인</Link>
+        </div>
+      </div>
+    </section>
   );
 }
 

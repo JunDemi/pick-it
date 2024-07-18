bc53542c7de1e7856bc2bc695e7df872abdd3576
JunDemi <jungwook3176@gmail.com>
2024-07-18 16:08:52 +0900

    feat: 로그인 사용자 정보 로컬 스토리지에 저장

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,36 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Home from './pages/Home';
-import '../src/assets/global.scss';
-import CreateGame from './pages/CreateGame';
-import Login from './pages/Login';
-import Register from './pages/Register';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import "../src/assets/global.scss";
+import CreateGame from "./pages/CreateGame";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import { AuthContext } from "./AuthContext";
 
 function App() {
+  //로그인 여부 확인
+  const { user }: any = AuthContext();
+  if (user.isLogin) {
+    localStorage.setItem( //로컬 스토리지 등록
+      "pickit-user",
+      JSON.stringify({
+        LoginToken: user.user.uid,
+        UserId: String(user.user.email).slice(
+          0,
+          String(user.user.email).indexOf("@") //이메일 도메인 제거
+        ),
+      })
+    );
+  }
   return (
     <BrowserRouter>
-      <Header/>
+      <Header />
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/create-game' element={<CreateGame/>}/>
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/register' element={<Register/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/create-game" element={<CreateGame />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,14 +10,18 @@ function Header() {
   //네비게이터
   const navigate = useNavigate();
   //로그인 상태 확인
-  const { user }: any = AuthContext();
+  const user: string | null = localStorage.getItem("pickit-user");
   //로그아웃 메소드
   const logOut = () => {
     //로그아웃 메소드
     signOut(auth);
+    //로컬스토리지 로그인 정보 삭제
+    localStorage.setItem(
+      "pickit-user",
+      "" //비어있는 값
+    );
     navigate("/");
   };
-
   const location = useLocation();
 
   //햄버거 메뉴 토글
@@ -25,16 +29,16 @@ function Header() {
 
   return (
     <>
-      <header className='header-container'>
-        <div className='header-logo'>
-          <Link to='/' className='logo'>
+      <header className="header-container">
+        <div className="header-logo">
+          <Link to="/" className="logo">
             PICKIT
           </Link>
         </div>
         <nav>
-          <ul className='main-menu'>
+          <ul className="main-menu">
             <li className={location.pathname === "/" ? "menu active" : "menu"}>
-              <a href='/'>대시보드</a>
+              <a href="/">대시보드</a>
             </li>
 
             <li
@@ -42,7 +46,7 @@ function Header() {
                 location.pathname === "/worldCup" ? "menu active" : "menu"
               }
             >
-              <a href='/'>월드컵 참여</a>
+              <a href="/">월드컵 참여</a>
             </li>
 
             <li
@@ -50,53 +54,53 @@ function Header() {
                 location.pathname === "/create-game" ? "menu active" : "menu"
               }
             >
-              <a href='/create-game'>월드컵 생성</a>
+              <a href="/create-game">월드컵 생성</a>
             </li>
           </ul>
 
-          <div className='user-menu'>
-            {user.isLogin ? (
-              <button className='login-menu' onClick={logOut}>
+          <div className="user-menu">
+            {user ? (
+              <button className="login-menu" onClick={logOut}>
                 <svg
-                  width='18'
-                  height='18'
-                  viewBox='0 0 20 20'
-                  fill='none'
-                  xmlns='http://www.w3.org/2000/svg'
+                  width="18"
+                  height="18"
+                  viewBox="0 0 20 20"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
                 >
                   <path
-                    d='M8.51172 4.13672L13.3086 8.93359C13.5898 9.21484 13.75 9.60156 13.75 10C13.75 10.3984 13.5898 10.7852 13.3086 11.0664L8.51172 15.8633C8.26172 16.1133 7.92578 16.25 7.57422 16.25C6.84375 16.25 6.25 15.6562 6.25 14.9258V12.5H1.25C0.558594 12.5 0 11.9414 0 11.25V8.75C0 8.05859 0.558594 7.5 1.25 7.5H6.25V5.07422C6.25 4.34375 6.84375 3.75 7.57422 3.75C7.92578 3.75 8.26172 3.89062 8.51172 4.13672ZM13.75 16.25H16.25C16.9414 16.25 17.5 15.6914 17.5 15V5C17.5 4.30859 16.9414 3.75 16.25 3.75H13.75C13.0586 3.75 12.5 3.19141 12.5 2.5C12.5 1.80859 13.0586 1.25 13.75 1.25H16.25C18.3203 1.25 20 2.92969 20 5V15C20 17.0703 18.3203 18.75 16.25 18.75H13.75C13.0586 18.75 12.5 18.1914 12.5 17.5C12.5 16.8086 13.0586 16.25 13.75 16.25Z'
-                    fill='black'
+                    d="M8.51172 4.13672L13.3086 8.93359C13.5898 9.21484 13.75 9.60156 13.75 10C13.75 10.3984 13.5898 10.7852 13.3086 11.0664L8.51172 15.8633C8.26172 16.1133 7.92578 16.25 7.57422 16.25C6.84375 16.25 6.25 15.6562 6.25 14.9258V12.5H1.25C0.558594 12.5 0 11.9414 0 11.25V8.75C0 8.05859 0.558594 7.5 1.25 7.5H6.25V5.07422C6.25 4.34375 6.84375 3.75 7.57422 3.75C7.92578 3.75 8.26172 3.89062 8.51172 4.13672ZM13.75 16.25H16.25C16.9414 16.25 17.5 15.6914 17.5 15V5C17.5 4.30859 16.9414 3.75 16.25 3.75H13.75C13.0586 3.75 12.5 3.19141 12.5 2.5C12.5 1.80859 13.0586 1.25 13.75 1.25H16.25C18.3203 1.25 20 2.92969 20 5V15C20 17.0703 18.3203 18.75 16.25 18.75H13.75C13.0586 18.75 12.5 18.1914 12.5 17.5C12.5 16.8086 13.0586 16.25 13.75 16.25Z"
+                    fill="black"
                   />
                 </svg>
                 <p>LOG OUT</p>
               </button>
             ) : (
-              <Link to='/login' className='login-menu'>
+              <Link to="/login" className="login-menu">
                 <svg
-                  viewBox='0 0 20 20'
-                  fill='none'
-                  xmlns='http://www.w3.org/2000/svg'
+                  viewBox="0 0 20 20"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
                 >
                   <path
-                    d='M8.51172 4.13672L13.3086 8.93359C13.5898 9.21484 13.75 9.60156 13.75 10C13.75 10.3984 13.5898 10.7852 13.3086 11.0664L8.51172 15.8633C8.26172 16.1133 7.92578 16.25 7.57422 16.25C6.84375 16.25 6.25 15.6562 6.25 14.9258V12.5H1.25C0.558594 12.5 0 11.9414 0 11.25V8.75C0 8.05859 0.558594 7.5 1.25 7.5H6.25V5.07422C6.25 4.34375 6.84375 3.75 7.57422 3.75C7.92578 3.75 8.26172 3.89062 8.51172 4.13672ZM13.75 16.25H16.25C16.9414 16.25 17.5 15.6914 17.5 15V5C17.5 4.30859 16.9414 3.75 16.25 3.75H13.75C13.0586 3.75 12.5 3.19141 12.5 2.5C12.5 1.80859 13.0586 1.25 13.75 1.25H16.25C18.3203 1.25 20 2.92969 20 5V15C20 17.0703 18.3203 18.75 16.25 18.75H13.75C13.0586 18.75 12.5 18.1914 12.5 17.5C12.5 16.8086 13.0586 16.25 13.75 16.25Z'
-                    fill='black'
+                    d="M8.51172 4.13672L13.3086 8.93359C13.5898 9.21484 13.75 9.60156 13.75 10C13.75 10.3984 13.5898 10.7852 13.3086 11.0664L8.51172 15.8633C8.26172 16.1133 7.92578 16.25 7.57422 16.25C6.84375 16.25 6.25 15.6562 6.25 14.9258V12.5H1.25C0.558594 12.5 0 11.9414 0 11.25V8.75C0 8.05859 0.558594 7.5 1.25 7.5H6.25V5.07422C6.25 4.34375 6.84375 3.75 7.57422 3.75C7.92578 3.75 8.26172 3.89062 8.51172 4.13672ZM13.75 16.25H16.25C16.9414 16.25 17.5 15.6914 17.5 15V5C17.5 4.30859 16.9414 3.75 16.25 3.75H13.75C13.0586 3.75 12.5 3.19141 12.5 2.5C12.5 1.80859 13.0586 1.25 13.75 1.25H16.25C18.3203 1.25 20 2.92969 20 5V15C20 17.0703 18.3203 18.75 16.25 18.75H13.75C13.0586 18.75 12.5 18.1914 12.5 17.5C12.5 16.8086 13.0586 16.25 13.75 16.25Z"
+                    fill="black"
                   />
                 </svg>
                 <p>LOGIN</p>
               </Link>
             )}
 
-            <div className='user-profile'>
-              {user.isLogin ? (
+            <div className="user-profile">
+              {user ? (
                 <>
-                  <div className='user-imgBox'></div>
+                  <div className="user-imgBox"></div>
                 </>
               ) : (
                 <>
                   <button
-                    type='button'
-                    className='non-member'
+                    type="button"
+                    className="non-member"
                     onClick={() => setUserPopupToggle(!userPopupToggle)}
                   >
                     <FaUser />

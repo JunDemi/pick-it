5e85143dde665a8ec5f33bbe667d1656cee95ee7
MIYEOK <lim5570@naver.com>
2024-07-22 23:59:18 +0900

    07.22 Feat. UserPopup 컴포넌트 추가 & Header 구조 수정 & App 컴포넌트 렌더링시마다 로컬스토리지에 저장된 회원 데이터 팝업창 props 전달하여 출력

--- src/components/UserPopup/UserPopup.tsx
+++ src/components/UserPopup/UserPopup.tsx
@@ -1,7 +1,117 @@
 import React from "react";
+import { FaUser } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+import { signOut } from "firebase/auth";
+import { auth } from "../../server/firebase";
+import { PopupUserData } from "../../types/Sign";
+import { PopupContext, ToggleContextType } from "../../context/PopupContext";
+import { motion } from "framer-motion";
 
-const UserPopup = () => {
-  return <div>UserPopup</div>;
+interface PropsUserData {
+  userData: PopupUserData;
+}
+
+const UserPopup = ({ userData }: PropsUserData) => {
+  //네비게이터
+  const navigate = useNavigate();
+
+  const { setUserPopupToggle }: ToggleContextType = PopupContext();
+
+  //로그아웃 메소드
+  const logOut = () => {
+    //로그아웃 메소드
+    signOut(auth);
+    //로컬스토리지 로그인 정보 삭제
+    localStorage.removeItem("pickit-user");
+    navigate("/");
+  };
+
+  const containerMotion = {
+    initial: {
+      opacity: 0,
+      y: 20,
+    },
+
+    animate: {
+      opacity: 1,
+      y: 0,
+
+      transition: {
+        staggerChildren: 0.5,
+        delayChildren: 1,
+        duration: 1,
+      },
+    },
+  };
+
+  const itemMotion = {
+    initial: {
+      opacity: 0,
+    },
+
+    animate: {
+      opacity: 1,
+    },
+  };
+
+  return (
+    <motion.div
+      className='userPopup-container'
+      variants={containerMotion}
+      initial='initial'
+      animate='animate'
+    >
+      <div className='userPopup-wrap'>
+        <motion.div className='user-profile' variants={itemMotion}>
+          <div className='profile-img'>
+            {userData.imgUrl === "default" ? (
+              <FaUser />
+            ) : (
+              <img
+                src={userData.imgUrl}
+                alt={`${userData.nickName} 님의 프로필 이미지`}
+              />
+            )}
+          </div>
+
+          <div className='profile-info'>
+            <div className='user-name'>
+              <h1>{`${userData.nickName}님 환영합니다.`}</h1>
+            </div>
+            <button
+              type='button'
+              className='logOut-button'
+              value='로그아웃'
+              onClick={() => logOut()}
+            >
+              로그아웃
+            </button>
+          </div>
+        </motion.div>
+
+        <motion.ul className='user-menu' variants={itemMotion}>
+          <li>
+            <button type='button' className='menu' value='마이페이지'>
+              마이페이지
+            </button>
+          </li>
+
+          <li>
+            <button
+              type='button'
+              className='menu'
+              value='페이지로 돌아가기'
+              onClick={() =>
+                setUserPopupToggle ? setUserPopupToggle(false) : "none"
+              }
+            >
+              페이지로 돌아가기
+            </button>
+          </li>
+        </motion.ul>
+      </div>
+    </motion.div>
+  );
 };
 
 export default UserPopup;
--- src/server/firebaseAuth.ts
+++ src/server/firebaseAuth.ts
@@ -6,6 +6,7 @@ import {
   deleteDoc,
   doc,
   documentId,
+  getDoc,
   getDocs,
   limit,
   orderBy,
@@ -14,7 +15,13 @@ import {
   where,
 } from "firebase/firestore";
 import { db } from "./firebase";
-import { browserLocalPersistence, getAuth, setPersistence, signInWithEmailAndPassword } from "firebase/auth";
+import {
+  browserLocalPersistence,
+  getAuth,
+  setPersistence,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { ref } from "firebase/storage";
 
 //auth 세션불러오기
 const auth = getAuth();
@@ -24,21 +31,47 @@ const authRef = collection(db, "users");
 // 파이어베이스 로그인
 export const signInPickit = async (userId: string, userPw: string) => {
   await setPersistence(auth, browserLocalPersistence) //로그인 정보들을 브라우저 로컬에 등록
-  .then(() => {
-    return signInWithEmailAndPassword(auth, userId, userPw);
-  })
-  .catch((error) => {
-    // Handle Errors here.
-    const errorCode = error.code;
-    const errorMessage = error.message;
-  });
-}
-// 회원가입 데이터 DB저장. 테이블 이름 = users
-export const getAuthenticInfo = (keyId: string, userId: string, userNickName: string, userImg: string) => {
-    addDoc(collection(db, "users"), {
-        keyId: keyId,
-        userId: userId,
-        userNickName: userNickName,
-        userImg: userImg
+    .then(() => {
+      return signInWithEmailAndPassword(auth, userId, userPw);
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+
+      console.log(errorCode, errorMessage);
     });
-  };
+};
+// 회원가입 데이터 DB저장. 테이블 이름 = users
+export const getAuthenticInfo = (
+  keyId: string,
+  userId: string,
+  userNickName: string,
+  userImg: string
+) => {
+  addDoc(collection(db, "users"), {
+    keyId: keyId,
+    userId: userId,
+    userNickName: userNickName,
+    userImg: userImg,
+  });
+};
+
+/* userPopup 컴포넌트 사용자 조회 */
+export const getUserData = async (userid: string) => {
+  // 사용자 컬렉션에 매개변수로 넘겨받은 userid값을 문서 필드값 userId와 비교하여 사용자 조회
+  const findRef = query(collection(db, "users"), where("userId", "==", userid));
+  const findIdDocs = await getDocs(findRef);
+
+  const profileImgUrl: any = [];
+
+  if (!findIdDocs.empty) {
+    findIdDocs.forEach((doc) => {
+      const profileImg = doc.data()["userImg"];
+      const nickName = doc.data()["userNickName"];
+      profileImgUrl.push(profileImg, nickName);
+    });
+
+    return profileImgUrl;
+  }
+};

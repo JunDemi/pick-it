eeaf8f6dac5ad88e108ab44f199a76826432b8be
JunDemi <jungwook3176@gmail.com>
2024-08-03 00:30:14 +0900

    feat: 리액트 쿼리 Context API(provider)생성

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { QueryClient, QueryClientProvider } from "react-query";
 import AuthProvider from "./context/AuthContext";
 import PopupProvider from "./context/PopupContext";
 import { Provider } from "react-redux";
 import store from "./store/store";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <Provider store={store}>
+    <QueryClientProvider client={queryClient}>
     <AuthProvider>
       <PopupProvider>
         <App />
       </PopupProvider>
     </AuthProvider>
+    </QueryClientProvider>
   </Provider>
 );

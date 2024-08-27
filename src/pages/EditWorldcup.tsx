9bd78abb5d333058ffff524c5b804fc203014511
JunDemi <jungwook3176@gmail.com>
2024-08-27 21:25:38 +0900

    feat: (월드컵수정) 각 이미지 썸네일에 label랩핑 및 input file 온체인지 핸들러 적용하여 프리뷰 동작 구현, objectURL을 사용하여 프리뷰 구현

--- src/pages/EditWorldcup.tsx
+++ src/pages/EditWorldcup.tsx
@@ -19,9 +19,17 @@ function EditWorldcup() {
     gameInfo: DocumentData;
   } | null>(null);
 
-  const [inputData, setInputData] = useState<WorldcupImage[]>();
+  const [inputData, setInputData] = useState<
+    {
+      fileIndex: number;
+      filePath: string;
+      fileName: string;
+      previewImg: File | null;
+    }[]
+  >();
   // 데이터 불러오기 로딩 동작
   const [isLoading, setIsLoading] = useState<boolean>(true);
+
   //유저 체크
   useEffect(() => {
     if (!user) {
@@ -48,6 +56,7 @@ function EditWorldcup() {
   ) => {
     if (inputData) {
       const newInputs = inputData.map((input) =>
+        //체인지를 준 텍스트의 인덱스와 배열 인덱스가 동일한 데이터의 fileName업데이트
         input.fileIndex === index
           ? { ...input, fileName: event.target.value }
           : input
@@ -56,7 +65,23 @@ function EditWorldcup() {
     }
   };
 
-  console.log(inputData);
+  const setOnChangeFile = (
+    index: number,
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    if (inputData) {
+      const newInputs = inputData.map((input) => {
+        //체인지를 준 파일의 인덱스와 배열 인덱스가 동일한 데이터의 프리뷰이미지 업데이트, 취소하면 null
+        return input.fileIndex === index
+          ? {
+              ...input,
+              previewImg: event.target.files ? event.target.files[0] : null,
+            }
+          : input;
+      });
+      setInputData(newInputs);
+    }
+  };
   return !isLoading && gameData && inputData ? (
     gameData.gameInfo.userId === parseUser.UserId ? (
       <div className="edit-worldcup-container">
@@ -72,12 +97,48 @@ function EditWorldcup() {
             </thead>
             <tbody>
               {inputData
-                .sort((indexA, indexB) => indexA.fileIndex - indexB.fileIndex)
-                .map((items) => (
+                //.sort((indexA, indexB) => indexA.fileIndex - indexB.fileIndex)
+                .map((items, number) => (
                   <tr key={items.fileIndex}>
-                    <td>{items.fileIndex + 1}</td>
+                    <td>{number + 1}</td>
                     <td>
-                      <img src={items.filePath} alt="" />
+                      <label>
+                        <div className="img-wrapper">
+                          <div className="icon-wrapper">
+                            <svg
+                              viewBox="0 0 80 80"
+                              fill="none"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              <path
+                                fillRule="evenodd"
+                                clipRule="evenodd"
+                                d="M75 70C75 72.76 72.76 75 70 75H57.08L38.66 56.3375L60 34.9975L75 49.9975V70ZM10 75C7.24 75 5 72.76 5 70V67.6525L24.8625 49.8625L50.0025 75H10ZM20 10C25.5225 10 30 14.4775 30 20C30 25.5225 25.5225 30 20 30C14.4775 30 10 25.5225 10 20C10 14.4775 14.4775 10 20 10ZM70 0H10C4.4775 0 0 4.4775 0 10V70C0 75.5225 4.4775 80 10 80H70C75.5225 80 80 75.5225 80 70V10C80 4.4775 75.5225 0 70 0ZM20 25C22.76 25 25 22.76 25 20C25 17.24 22.76 15 20 15C17.24 15 15 17.24 15 20C15 22.76 17.24 25 20 25Z"
+                              />
+                            </svg>
+                            이미지 변경하기
+                          </div>
+
+                          <img
+                            src={
+                              items.previewImg
+                                ? (URL.createObjectURL(
+                                    items.previewImg
+                                  ) as string)
+                                : items.filePath
+                            }
+                            alt=""
+                          />
+                        </div>
+
+                        <input
+                          type="file"
+                          accept="image/*"
+                          onChange={(event) =>
+                            setOnChangeFile(items.fileIndex, event)
+                          }
+                        />
+                      </label>
                     </td>
                     <td>
                       <input

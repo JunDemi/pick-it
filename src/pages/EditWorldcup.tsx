b580d6823a21ef4d74d466a81419a5b00cfc13b7
JunDemi <jungwook3176@gmail.com>
2024-08-27 20:17:54 +0900

    feat: 월드컵 수정 테이블UI 구현, 월드컵 데이터 state에 저장 후 이미지 목록은 다른 state에 별도 저장하여 온체인지 동작 추가

--- src/pages/EditWorldcup.tsx
+++ src/pages/EditWorldcup.tsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { findSelectWorldcup } from "../server/firebaseWorldcup";
 import { DocumentData } from "firebase/firestore";
+import "../assets/MyPage/editWorldcup.scss";
+import { WorldcupImage } from "../types/Worldcup";
 
 function EditWorldcup() {
   // 동적 라우팅으로 전송받은 월드컵 아이디 값 조회
@@ -16,6 +18,8 @@ function EditWorldcup() {
     gameId: string;
     gameInfo: DocumentData;
   } | null>(null);
+
+  const [inputData, setInputData] = useState<WorldcupImage[]>();
   // 데이터 불러오기 로딩 동작
   const [isLoading, setIsLoading] = useState<boolean>(true);
   //유저 체크
@@ -25,18 +29,74 @@ function EditWorldcup() {
       navigate("/login");
     }
   }, [navigate, user]);
+  //데이터 불러오기
   useEffect(() => {
     if (gameId) {
       findSelectWorldcup(gameId)
-        .then((result) => setGameData(result))
+        .then((result) => {
+          setGameData(result); //전체 데이터 저장
+          setInputData(result?.gameInfo.worldcupImages); //이미지 목록 데이터만 저장
+        })
         .then(() => setIsLoading(false));
     }
   }, [gameId]);
 
-  return !isLoading && gameData ? (
+  //이미지 목록 이름 텍스트 변경 온체인지 핸들러
+  const handleInputChange = (
+    index: number,
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    if (inputData) {
+      const newInputs = inputData.map((input) =>
+        input.fileIndex === index
+          ? { ...input, fileName: event.target.value }
+          : input
+      );
+      setInputData(newInputs);
+    }
+  };
+
+  console.log(inputData);
+  return !isLoading && gameData && inputData ? (
     gameData.gameInfo.userId === parseUser.UserId ? (
-      <div>
-        {gameData.gameInfo.userId}, {parseUser.UserId} 맞음
+      <div className="edit-worldcup-container">
+        <section className="update-section">
+          <table>
+            <thead>
+              <tr>
+                <td>순번</td>
+                <td>이미지</td>
+                <td>이름</td>
+                <td>필드 삭제</td>
+              </tr>
+            </thead>
+            <tbody>
+              {inputData
+                .sort((indexA, indexB) => indexA.fileIndex - indexB.fileIndex)
+                .map((items) => (
+                  <tr key={items.fileIndex}>
+                    <td>{items.fileIndex + 1}</td>
+                    <td>
+                      <img src={items.filePath} alt="" />
+                    </td>
+                    <td>
+                      <input
+                        type="text"
+                        value={items.fileName}
+                        onChange={(event) =>
+                          handleInputChange(items.fileIndex, event)
+                        }
+                      />
+                    </td>
+                    <td>
+                      <button>삭제</button>
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </section>
+        <aside></aside>
       </div>
     ) : (
       <div className="before-game-message">

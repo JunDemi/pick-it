9608891da496df5f0c612bb18135760193ce45d0
JunDemi <jungwook3176@gmail.com>
2024-10-13 19:29:42 +0900

    feat: 커뮤니티 게시물 intersectionObserver로 무한스크롤 구현, inview와 hasNextPage조건으로 로딩 UI 삽입

--- src/pages/Community.tsx
+++ src/pages/Community.tsx
@@ -1,18 +1,19 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import PopRank from "../components/Home/Banner/PopRank";
 import "../assets/Community/community.scss";
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { getCommunityList } from "../server/firebaseCommunity";
-import { getUserData } from "../server/firebaseAuth";
+import { useInView } from "react-intersection-observer";
 
 function Community() {
+  //인터섹션 옵저버 훅
+  const { ref, inView } = useInView();
   //리액트 쿼리 훅
   const {
     status,
-    error,
     data: communityList,
     fetchNextPage, //다음 페이지 불러오기,
-    isFetchingNextPage, //다음 페이지 불러오는 중
+    hasNextPage,
     refetch,
   } = useInfiniteQuery({
     queryKey: ["community_api"],
@@ -21,9 +22,29 @@ function Community() {
     getNextPageParam: (lastPage) => lastPage.nextPage, //fetchNextPage가 작동하면 nextPage로 pageParam값 증가
   });
 
+  //무한스크롤 시 로딩 상태
+  const [newLoading, setNewLoading] = useState<boolean>(false);
+  //ref에 닿으면 무한 스크롤 1회 작동
+  useEffect(() => {
+    //스크롤 끝에 닿고 다음 페이지가 존재할 경우 로딩 및 0.6초뒤 페이지 불러오기
+    if (inView && hasNextPage) {
+      setNewLoading(true);
+      setTimeout(() => {
+        fetchNextPage();
+      }, 500);
+    } else {
+      setNewLoading(false);
+    }
+  }, [inView, fetchNextPage]);
 
   return status === "pending" ? (
-    <div>로딩중...</div>
+    <div className="community-loading">
+      <h2>게시글을 불러오는 중입니다...</h2>
+      <div className="loading-spiner">
+        <hr />
+        <div />
+      </div>
+    </div>
   ) : status === "error" ? (
     <div>에러</div>
   ) : (
@@ -34,66 +55,88 @@ function Community() {
           작성할 수 있습니다. &#8251;
         </h1>
         {communityList.pages.map((page, index) => (
-        <main key={page.currentPage}>
-        {page.data.map((items) => items && (
-          <div key={items.communityId} className="community-card">
-            <div className="card-head">
-              <img src={items.userProfile === "default" ? "/images/user.png" : items.userProfile} alt="" />
-              <div>
-                <h2>{items.communityTitle}</h2>
-                <p>{items.userName}</p>
-              </div>
-            </div>
-            <div className="card-question">
-              {items.communitySubTitle}
-            </div>
-            <div className="card-images">
-              <div>
-                <img src={items.firstImg.filePath} alt="" />
-                <p>{items.firstImg.fileName}</p>
-              </div>
-              <div>
-              <img src={items.secondImg.filePath} alt="" />
-              <p>{items.secondImg.fileName}</p>
-              </div>
-            </div>
-            <div className="card-hearts">
-              <div>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1"
-                  stroke="#ff0000"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
-                  />
-                </svg>
-                좋아요 {items.heart.length}개
-              </div>
-              <span>댓글 모두 보기</span>
-            </div>
-            <div className="card-comments">
-              {[...Array(3)].map((x, j) => (
-                <div key={j} className="comment-row">
-                  <div className="profile">
-                    <img src="/images/user.png" alt="" />
-                    민혀기
+          <main key={page.currentPage}>
+            {page.data.map(
+              (items) =>
+                items && (
+                  <div key={items.communityId} className="community-card">
+                    <div className="card-head">
+                      <img
+                        src={
+                          items.userProfile === "default"
+                            ? "/images/user.png"
+                            : items.userProfile
+                        }
+                        alt=""
+                      />
+                      <div>
+                        <h2>{items.communityTitle}</h2>
+                        <p>{items.userName}</p>
+                      </div>
+                    </div>
+                    <div className="card-question">
+                      {items.communitySubTitle}
+                    </div>
+                    <div className="card-images">
+                      <div>
+                        <img src={items.firstImg.filePath} alt="" />
+                        <p>{items.firstImg.fileName}</p>
+                      </div>
+                      <div>
+                        <img src={items.secondImg.filePath} alt="" />
+                        <p>{items.secondImg.fileName}</p>
+                      </div>
+                    </div>
+                    <div className="card-hearts">
+                      <div>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth="1"
+                          stroke="#ff0000"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
+                          />
+                        </svg>
+                        좋아요 {items.heart.length}개
+                      </div>
+                      <span>댓글 모두 보기</span>
+                    </div>
+                    <div className="card-comments">
+                      {[...Array(3)].map((x, j) => (
+                        <div key={j} className="comment-row">
+                          <div className="profile">
+                            <img src="/images/user.png" alt="" />
+                            민혀기
+                          </div>
+                          <div className="text">
+                            <h3>
+                              INVU 뮤비 보면 이런 고민 못하지...너무 이쁘던데
+                            </h3>
+                            <p>1일 전</p>
+                          </div>
+                        </div>
+                      ))}
+                    </div>
                   </div>
-                  <div className="text">
-                    <h3>INVU 뮤비 보면 이런 고민 못하지...너무 이쁘던데</h3>
-                    <p>1일 전</p>
-                  </div>
-                </div>
-              ))}
-            </div>
-          </div>
-        ))}
+                )
+            )}
           </main>
         ))}
+        <div ref={ref}>
+          {newLoading && (
+            <div className="infi-scroll-wrapper">
+            <div className="loading-spiner">
+              <hr />
+              <div />
+            </div>
+            </div>
+          )}
+        </div>
       </div>
       <div className="pop-section">
         <PopRank />

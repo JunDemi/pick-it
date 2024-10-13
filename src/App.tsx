d79237b912ee56f843aa9d6764f4b796d3145950
JunDemi <jungwook3176@gmail.com>
2024-10-13 17:43:02 +0900

    feat: 리액트쿼리 useInfinityQuery훅 사용하여 커뮤니티 게시글 리스트 데이터 fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter, Routes, Route, useLocation} from "react-router-dom";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 import Home from "./pages/Home";
 import "../src/assets/global.scss";
 import "./assets/userPopup/userPopup.scss";
--- src/server/firebaseCommunity.ts
+++ src/server/firebaseCommunity.ts
@@ -1,7 +1,10 @@
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, getDocs, orderBy, query } from "firebase/firestore";
 import { db } from "./firebase";
 import { WorldcupImage } from "../types/Worldcup";
+//파이어베이스 DB연동
+const communityRef = collection(db, "community");
 
+//커뮤니티 글 작성
 export const getCreateCommunity = async (
   gameId: string,
   userId: string,
@@ -17,7 +20,7 @@ export const getCreateCommunity = async (
     gameId: gameId,
     userId: userId,
     communityTitle: title,
-    communitySubTtile: subTitle,
+    communitySubTitle: subTitle,
     firstImg: first,
     secondImg: second,
     heart: [],
@@ -25,3 +28,30 @@ export const getCreateCommunity = async (
     updateAt: currentTime,
   });
 };
+
+//커뮤니티 리스트 불러오기
+export const getCommunityList = async({pageParam}: {
+  pageParam: number;
+}) => {
+  const LIMIT = 2;
+  //최신순 정렬 쿼리
+  const communityQuery = query(
+    communityRef,
+    orderBy("createAt", "desc")
+  );
+  //getDocs후 docs객체 할당
+  const getData = await getDocs(communityQuery).then((res) => {
+    return res.docs;
+  });
+   //클라이언트에 반환시킬 전체 결과값
+   const results = getData.map((data) => {
+    return { communityId: data.id, communityInfo: data.data() };
+  });
+
+  return {
+    data: results.slice(pageParam, pageParam + LIMIT),
+    currentPage: pageParam,
+    nextPage:
+      pageParam + LIMIT < results.length ? pageParam + LIMIT : null,
+  };
+}
